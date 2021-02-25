160675f47eda15959fa2e5761444659549149e12
khaizon <61576319+khaizon@users.noreply.github.com>
2021-02-26 01:52:18 +0800

    define register and login function

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,15 +1,28 @@
-const gql = require('graphql-tag');
+const gql = require("graphql-tag");
 
 module.exports = gql`
-    type Auditor {
-        auditorId: String!
-        name: String!
-        email: String!
-        tenants: [String]!
-    }
+  type Auditor {
+    auditorId: String!
+    name: String!
+    email: String!
+    tenants: [String]!
+  }
 
-    type Query {
-        getAllAuditors: [Auditor]
-    }
-    
-`
+  type Query {
+    getAllAuditors: [Auditor]
+  }
+
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+
+  type Mutation {
+      register(registerInput: RegisterInput): User!
+      login(username: String!, password: String!): User!
+  }
+`;
+
+//RegisterInput : currently has 4 fields, but yet to decide on the way that registration should work
