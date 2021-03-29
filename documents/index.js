1e3dea9a6beb64f9fd4b5d21caea44cbd954c923
JustANameBrooo <54141168+JustANameBrooo@users.noreply.github.com>
2021-03-29 13:18:05 +0800

    pdf

--- documents/index.js
+++ documents/index.js
@@ -1,6 +1,110 @@
 module.exports = (checklistData) => {
-  console.log(checklistData.checklistData.total);
-  const today = new Date();
+   console.log(checklistData.checklistData.total);
+   const today = new Date();
+
+   // <tr class="item">
+   //          <td>First item: ${checklistData.checklistData.item1}</td>
+   //          <td>${checklistData.checklistData.item1score}</td>
+   //       </tr>
+   //       <tr class="item">
+   //          <td>First item: ${checklistData.checklistData.item1}</td>
+   //          <td>${checklistData.checklistData.item1score}</td>
+   //       </tr>
+
+   var checklist = ``;
+
+  const lineItems = [
+     {
+        name: 'name 1',
+        score: 'score 1',
+     },
+     {
+        name: 'name 2',
+        score: 'score 2',
+     }
+   ];
+
+//    for (catCount = 0; catCount < checklistData.length; catCount++) {
+//       checklist += `
+//        <tr class="heading">
+//           <td>First Heading: ${checklistData[catCount].category.categoryName}</td>
+//        </tr>`
+
+//       for (subcatCount = 0; subcatCount < checklistData[catCount].subcategories.length; subcatCount++) {
+         
+//          checklist += `
+//          <tr class="item">
+//             <td>First item: ${checklistData[catCount].subcategories[subcatCount].subcategoryName}</td>
+//          </tr>`
+
+//          for (itemCount = 0; itemCount < checklistData[catCount].subcategories[subcatCount].lineItems.length; itemCount++) {
+            
+//             checklist += `
+//             <tr class="item">
+//                <td>First item: ${checklistData[catCount].subcategories[subcatCount].lineItems[itemCount].lineItemName}</td>
+//             </tr>`
+
+//          }
+//       }
+//    }
+  
+//    checklistData.checklistData.map((category) => {
+//      checklist += `
+//       <tr class="heading">
+//          <td>First item: ${category.categoryName}</td>
+//          <td>${parseInt(category.total)}</td>
+//       </tr>
+//       `
+//       category.subcategories.map(subcategory => {
+//          checklist += `
+//             <tr class="item">
+//                <td>First item: ${subcategory.subcategoryName}</td>
+//             </tr>
+            
+//          `
+//          subcategory.lineItems.map(lineItem => {
+//             // some string
+//             checklist += `
+//             <tr class="sub">
+//                <td>${lineItem.lineItemName}</td>
+//             </tr>
+//             `
+//          })
+//       })
+//   })
+
+   data.data.checklist.map((category) => {
+      checklist += `
+      <tr class="heading">
+         <td>First item: ${category.category}</td>
+         <td>${parseInt(category.score)}</td>
+      </tr>
+      `
+      category.subcategories.map(subcategory => {
+         checklist += `
+            <tr class="item">
+               <td>First item: ${subcategory.subcategory}</td>
+               <td>${parseInt(subcategory.subcatScore)}</td>
+            </tr>
+            
+         `
+         subcategory.lineItems.map(lineItem => {
+            checklist += `
+            <tr class="sub">
+               <td>${lineItem.lineItem}</td>
+            </tr>
+            `
+               lineItem.images.map(image => {
+                  checklist += `
+                     <tr class="sub">
+                        <td>${image.id}</td>
+                     </tr>
+                     `
+               })
+         })
+      })
+   })
+
   return `
   <!doctype html>
   <html>
@@ -59,6 +163,12 @@ module.exports = (checklistData) => {
            .invoice-box table tr.item td {
            border-bottom: 1px solid #eee;
            }
+           .invoice-box table tr.sub td {
+            background-color: #F0F8FF ;
+            padding-right: 10px
+            padding-left: 10px
+            border: 1px solid #eee;
+            }
            .invoice-box table tr.item.last td {
            border-bottom: none;
            }
@@ -105,35 +215,24 @@ module.exports = (checklistData) => {
                         </tr>
                        <tr>
                           <td>
-                             Tenant Id: ${checklistData.checklistData.somth}
+                             Tenant Id: ${data.data.tenantId}
                           </td>
                        </tr>
                        <tr>
                         <td>
-                           Auditor(s): ${checklistData.checklistData.somth}
+                           Auditor(s): ${data.data.type}
                         </td>
                        </tr>
                     </table>
                  </td>
               </tr>
-
-              <tr class="heading">
-                 <td>Hygiene and cleaniness:</td>
-                 <td>Score</td>
-              </tr>
-              <tr class="item">
-                 <td>First item: ${checklistData.checklistData.item1}</td>
-                 <td>${checklistData.checklistData.item1score}</td>
-              </tr>
-              <tr class="item">
-                 <td>Second item: ${checklistData.checklistData.item2}</td>
-                 <td>${checklistData.checklistData.item2score}</td>
-              </tr>
+               ${checklist}
+               
            </table>
 
            <br />
            <h1 class="justify-center">Total score: ${
-             parseInt(checklistData.checklistData.total)
+             parseInt(data.data.auditScore)
            }</h1>
         </div>
      </body>
