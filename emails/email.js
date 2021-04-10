14a33ed513d41b5135bc043822bacddd65cf5da5
khaizon <kaifeng_toh@mymail.sutd.edu.sg>
2021-04-10 15:45:49 +0800

    try changing dir to ~/../result.pdf

--- emails/email.js
+++ emails/email.js
@@ -62,7 +62,7 @@ const sendEmail = (addressee, remarks) =>{
             filename: 'result.pdf',
             // path: 'C:/Users/Windows/ESCProject/sra-server/result.pdf',
             // path: 'C:/Users/tohka/Documents/SingHealth/sra-server/result.pdf',
-            path: `${__dirname}/../result.pdf`,
+            path: `~/../result.pdf`,
             // path: '/Users/nicoleyu/Developer/ESC/sra-server/result.pdf',
             contentType: 'aplication/pdf',
         }]
