1f327feeebb258abe072263c81219d79bbd47c9e
khaizon <kaifeng_toh@mymail.sutd.edu.sg>
2021-04-13 15:28:35 +0800

    email

--- util/validators.js
+++ util/validators.js
@@ -49,9 +49,9 @@ module.exports.validateCreateAuditorInput = (
     errors.email = 'Email must not be empty';
   } else {
     const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-    if (!email.match(regEx)) {
-      errors.email = 'Email must be a valid email address';
-    }
+    // if (!email.match(regEx)) {
+    //   errors.email = 'Email must be a valid email address';
+    // }
   }
 
   return {
